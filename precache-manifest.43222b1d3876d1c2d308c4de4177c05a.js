42e860f9a5662c34b9238e19e43b55a3526db3df
yogesh.matta29 <yogesh.matta29@gmail.com>
2020-09-22 01:02:59 +0530

    Updates

---
+++ precache-manifest.43222b1d3876d1c2d308c4de4177c05a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "add42b14bc1102fee8c173817e37dc03",
+    "url": "/shoppingcart/index.html"
+  },
+  {
+    "revision": "ff58d3f604b6f5a2172b",
+    "url": "/shoppingcart/static/css/main.5cea600e.chunk.css"
+  },
+  {
+    "revision": "d4cad3672fe43730d569",
+    "url": "/shoppingcart/static/js/2.97b9095d.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/shoppingcart/static/js/2.97b9095d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff58d3f604b6f5a2172b",
+    "url": "/shoppingcart/static/js/main.e18480f1.chunk.js"
+  },
+  {
+    "revision": "14f81b8085354ebaffb5",
+    "url": "/shoppingcart/static/js/runtime-main.5877305d.js"
+  }
+]);
